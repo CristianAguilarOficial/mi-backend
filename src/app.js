d80f1dbb23228ac6ebb15aff9955a9e02611556a
CristianAguilarOficial <aguilarcristian060@gmail.com>
2025-04-20 13:36:34 -0500

    actualizar app.js y index.js para el deploy

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
-//exportar express
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
-import authRoutes from "./routes/auth.routes.js";
-import taksRoutes from "./routes/task.routes.js";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+
 const app = express();
+
+// Leer la URL del frontend desde variables de entorno (o localhost)
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
     credentials: true,
   })
 );
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", authRoutes);
-app.use("/api", taksRoutes);
+app.use("/api", taskRoutes);
+
 export default app;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import app from "./app.js";
 import { connectDB } from "./db.js";
 
 connectDB();
-app.listen(3000);
-console.log("servet on port", 3000);
+
+// Usar el PORT de Railway o 3000 en local
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
